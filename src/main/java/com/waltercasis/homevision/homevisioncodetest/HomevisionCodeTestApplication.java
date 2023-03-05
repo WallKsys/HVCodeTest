f2e54eaa6e90bdb687babbf86827edd7dd571e5f
WallKsys <sith.walter@gmail.com>
2023-03-05 12:40:45 -0300

    initial commit

---
+++ src/main/java/com/waltercasis/homevision/homevisioncodetest/HomevisionCodeTestApplication.java
@@ -0,0 +1,13 @@
+package com.waltercasis.homevision.homevisioncodetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomevisionCodeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomevisionCodeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waltercasis/homevision/homevisioncodetest/HomevisionCodeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waltercasis.homevision.homevisioncodetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomevisionCodeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
